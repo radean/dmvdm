aee4c0edf7d6b2c1345d8ff10857776aa9b00da9
radean <radeanf@gmail.com>
2018-07-12 15:43:01 +0500

    Added SMTP

--- scripts/contactMail.php
+++ scripts/contactMail.php
@@ -1,24 +1,67 @@
+
+
+
 <?php
-// Compose the mail
-$to = "info@vdm.com.pk";
-$subject = "DigiGon™ Enquiry";
-$headers = "From: " . strip_tags($_POST['email']) . "\r\n";
-$headers .= "Reply-To: ". strip_tags($_POST['email']) . "\r\n";
-$headers .= "CC: black@radean.xyz\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-$message = '<html><body>';
-$message .= '<h2>DigiGon™ Enquiry Form</h2>';
-$message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
-$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_POST['fullname']) . "</td></tr>";
-$message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['email']) . "</td></tr>";
-$message .= "<tr><td><strong>Contact:</strong> </td><td>" . strip_tags($_POST['contact']) . "</td></tr>";
-//$message .= "<tr><td><strong>Address:</strong> </td><td>" . strip_tags($_POST['address']) . "</td></tr>";
-$message .= "<tr><td><strong>Message:</strong> </td><td>" . strip_tags($_POST['message']) . "</td></tr>";
-$message .= "</table>";
-$message .= "</body></html>";
-if(mail($to, $subject, $message, $headers)) {
-    header( 'Location: http://absaco.com' ) ;
-} else {
-    echo "Server Error RedCheck";
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+//    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'mx1.hostinger.in';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'digigon@vdm.com.pk';                 // SMTP username
+    $mail->Password = 'Vision123';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('digigon@vdm.com.pk', 'Digigon Enquiry');
+    $mail->addAddress('radeanf@gmail.com', 'Ayaz vayani');     // Add a recipient
+//    $mail->addAddress('ellen@example.com');               // Name is optional
+//    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('info@vdm.com.pk');
+//    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+//    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $message = '<html><body>';
+    $message .= '<h2>DigiGon™ Enquiry Form</h2>';
+    $message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+    $message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_POST['full_name']) . "</td></tr>";
+    $message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['email']) . "</td></tr>";
+    $message .= "<tr><td><strong>Contact:</strong> </td><td>" . strip_tags($_POST['contact']) . "</td></tr>";
+    $message .= "<tr><td><strong>Message:</strong> </td><td>" . strip_tags($_POST['message']) . "</td></tr>";
+    $message .= "</table>";
+    $message .= "</body></html>";
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Digigon Enquiry';
+    $mail->Body    = $message;
+
+
+//    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Thank you, Your Message has been sent. We will contact you soon';
+    echo ' this page will redirect in 7 sec to digigon.vdm.com.pk';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
 }
+?>
+
+<script type="application/javascript">
+    var url = "http://digigon.vdm.com.pk"
+    setTimeout(function () {
+        window.location.href = url
+    },7000)
+</script>
